a35931ccfe259ed6cf4989e81e05617685b2449b
Jin Zhao <sunrisemoment@outlook.com>
2020-10-20 05:44:29 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '08c0ad702f0d625c05211fb35b8913ad'),
+    },
+  },
+});
